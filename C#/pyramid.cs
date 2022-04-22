57b706c914be3cca9e5a446d510614b723407f94
Andrea S <71943777+andrea-bot@users.noreply.github.com>
2022-04-21 21:12:21 -0700

    Add files via upload

---
+++ C#/pyramid.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numberoflayer = 6, Space, Number;
+Console.WriteLine("Print paramid");
+for (int i = 1; i <= numberoflayer; i++)   
+{
+    for (Space = 1; Space <= (numberoflayer - i); Space++) 
+        Console.Write(" ");
+    for (Number = 1; Number <= i; Number++) 
+        Console.Write('*');
+    for (Number = (i - 1); Number >= 1; Number--)  
+        Console.Write('*');
+    Console.WriteLine();
+}
